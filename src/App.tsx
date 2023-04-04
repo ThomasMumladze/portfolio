60533cc433ff5d6ffc8496a54d8d9fcf17064c56
ThomasMumladze <mumladzea4@mail.com>
2023-04-04 17:33:19 +0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import PortfolioApp from "./components/PortfolioApp";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <header id="header">
+                <div className="header_body">
+                    <div className="TITLE">
+                        <h1>Front End Developer</h1>
+                    </div>
+                    <Navigation />
+                </div>
+            </header>
+            <Routes>
+                <Route path="/" element={<PortfolioApp />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;

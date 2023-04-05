f99b67fc9cd3d5f5a68a773b9fd5ffa2f324a807
ThomasMumladze <mumladzea4@mail.com>
2023-04-05 17:29:10 +0400

    skill component

---
+++ src/components/skillContainer/SkillContainer.tsx
@@ -0,0 +1,15 @@
+import "./skillContainer.scss";
+const SkillContainer = () => {
+    return (
+        <div className="skillContainer">
+            <div className="skills_title">
+                <h2>language</h2>
+            </div>
+            <div className="skill_description">
+                <span>javaScript Python TypeScript</span>
+            </div>
+        </div>
+    );
+};
+
+export default SkillContainer;

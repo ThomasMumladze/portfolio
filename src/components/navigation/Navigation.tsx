811534f62463c8e853a3c91f0e38455ad5027650
ThomasMumladze <mumladzea4@mail.com>
2023-04-05 13:35:20 +0400

    navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,18 +1,27 @@
 import "./navigation.scss";
 import { Link } from "react-router-dom";
-const Navigation = () => {
+
+// ========== Interfaces ========== //
+interface Props {
+    menuClassName: string;
+    menuLink_1: string;
+    menuLink_2: string;
+    menuLink_3: string;
+}
+const Navigation = (props: Props) => {
+    const { menuClassName, menuLink_1, menuLink_2, menuLink_3 } = props;
     return (
         <div className="navigation">
             <nav>
-                <ul>
+                <ul className={menuClassName}>
                     <li>
-                        <Link to={"/"}>home</Link>
+                        <Link to={"/"}>{menuLink_1}</Link>
                     </li>
                     <li>
-                        <Link to={"/resume"}>resume</Link>
+                        <Link to={"/resume"}>{menuLink_2}</Link>
                     </li>
                     <li>
-                        <Link to={"/project"}>project</Link>
+                        <Link to={"/project"}>{menuLink_3}</Link>
                     </li>
                 </ul>
             </nav>

93b53cd1ca19c414f9413b8b81b0f6f9a7752151
ThomasMumladze <mumladzea4@mail.com>
2023-04-06 11:51:45 +0400

    code compressed

--- src/pages/homePage/PortfolioApp.tsx
+++ src/pages/homePage/PortfolioApp.tsx
@@ -1,58 +1,22 @@
 import "./PortfolioApp.scss";
-import React from "react";
 
-// ========== COMPONENTS ========== //
-import Button from "../../components/button/Button";
-import ProjectCard from "../../components/projectCard/ProjectCard";
-// ========== SECTIONS
-import Skills from "../../components/@SECTIONS/skills/Skills";
-import PersonSection from "../../components/@SECTIONS/personSection/Person";
-
-// ========== LINKS ========== //
-import { Link } from "react-router-dom";
-
-// ========== HOOKS ========== //
-import { useState } from "react";
-
-// TEST IMAGE
-import image01 from "../../assets/images/1.png";
-import image02 from "../../assets/images/213123.png";
+// ========== SECTIONS ========== //
 import ProjectSection from "../../components/@SECTIONS/projectSection/ProjectSection";
+import SkillsSection from "../../components/@SECTIONS/skillsSection/SkillsSection";
+import PersonSection from "../../components/@SECTIONS/personSection/Person";
+import ServicesSection from "../../components/@SECTIONS/servicesSection/ServicesSection";
+
 const PortfolioApp = () => {
-    // TEST STATE
-    const [project, setProject] = useState([
-        {
-            id: 0,
-            liveServer: true,
-            codeSource: true,
-            projectImage: image01,
-            projectLink: "https://github-usersearch-app.netlify.app/",
-            projectSourceLink:
-                "https://github.com/ThomasMumladze/GitHub-UserSearch-Application",
-            projectTechnologies: "React.js",
-            projectTitle: "GIT Hub User Search",
-            projectAbout: "Searching gitHub User By User Names",
-        },
-        {
-            id: 1,
-            liveServer: false,
-            codeSource: true,
-            projectImage: image02,
-            projectLink: "",
-            projectSourceLink:
-                "https://github.com/ThomasMumladze/Python-Basic-Proejcts",
-            projectTechnologies: "Python",
-            projectTitle: "Python Basic Project",
-            projectAbout: "completed basic project on python",
-        },
-    ]);
-    // ----------------------------------------------------------------------------- //
     return (
         <div className="portfolioApp">
+            {/* ========== PERSON SECTION ========== */}
             <PersonSection />
+            {/* ========== SERVICES SECTION ========== */}
+            <ServicesSection />
+            {/* ========== PROJECT SECTION ========== */}
             <ProjectSection />
             {/* ========== SKILLS SECTION ========== */}
-            <Skills />
+            <SkillsSection />
         </div>
     );
 };
